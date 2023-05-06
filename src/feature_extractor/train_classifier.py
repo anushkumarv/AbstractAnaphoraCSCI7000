b08fefb400d7ecb15046df1a934f2895ebf777d6
anushkumarv <anush070@gmail.com>
2023-05-06 16:44:41 -0600

    fixed bugs

--- src/feature_extractor/train_classifier.py
+++ src/feature_extractor/train_classifier.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from feature_extractor_classifier import extract_features_classifier, load_data
 from report_results_helper import report_results
-from feature_extractor_antecedent_conll import extract_classifier_features
+from feature_extractor_classifier_conll import extract_classifier_features as extract_classifier_features_conll
 
 def train_and_report(args):
     data = ['./../../data/Resisting_rhetorics_of_language_endangerment/annotation.json','./../../data/Talking_Indian/annotation.json','./../../data/Language_Ideology_Agency_and_Heritage_Language_Insecurity_across_Immigrant_Generations/annotation.json','./../../data/Kimberley_language_resourse_center/annotation.json']
@@ -17,12 +17,13 @@ def train_and_report(args):
         df_temp = pd.DataFrame(item, columns = ['pronoun', 'token_pos','verb_presence','parent_lemma_verb','parent_lemma','parent_label','negated_parent','parent_transitivity','pronoun_path','is_abs_anph'])
         df = df.append(df_temp)
 
-    if argparse.conll:
+    if args.use_conll == "True":
         print("Extract features from conll")
-        df_conll = extract_classifier_features()
+        df_conll = pd.DataFrame(extract_classifier_features_conll(), columns = ['pronoun', 'token_pos','verb_presence','parent_lemma_verb','parent_lemma','parent_label','negated_parent','parent_transitivity','pronoun_path','is_abs_anph'])
         df = df.append(df_conll)
+
     print('Train test split')
-    train, test = train_test_split(df, test_size=0.2, stratify=df['is_abs_anph'])
+    train, test = train_test_split(df, test_size=0.2, stratify=df['is_abs_anph'], random_state=25)
 
     y_train, x_train = train['is_abs_anph'], train.drop('is_abs_anph', axis=1)
     y_test, x_test = test['is_abs_anph'], test.drop('is_abs_anph', axis=1)
@@ -42,16 +43,27 @@ def train_and_report(args):
     results_me = classifier_me.classify_many(x_test_lst)
     print(results_me)
 
+    print("This abstract_anaphora")
+    print(len(df[(df['is_abs_anph'] == True) & (df['pronoun'] == 'This')]) + len(df[(df['is_abs_anph'] == True) & (df['pronoun'] == 'this')]))
+    print("This non abstract_anaphora")
+    print(len(df[(df['is_abs_anph'] == False) & (df['pronoun'] == 'This')]) + len(df[(df['is_abs_anph'] == False) & (df['pronoun'] == 'this')]))
+    print("That abstract_anaphora")
+    print(len(df[(df['is_abs_anph'] == True) & (df['pronoun'] == 'That')]) + len(df[(df['is_abs_anph'] == True) & (df['pronoun'] == 'that')]))
+    print("That non abstract_anaphora")
+    print(len(df[(df['is_abs_anph'] == False) & (df['pronoun'] == 'That')]) + len(df[(df['is_abs_anph'] == False) & (df['pronoun'] == 'that')]))
 
     print("Length of training dataset", len(train))
     print("Length of positive datapoints", len(train[train['is_abs_anph'] == True]))
     print("Percentage of positive datapoints", len(train[train['is_abs_anph'] == True]) / len(train))
+    print("Length of test dataset", len(test))
+
 
     report_results("## Naive Bayes Classifier ##", y_test_lst, results_nb)
     report_results("## Max Ent Classifier ##", y_test_lst, results_me)
 
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--conll', type=bool, required=True)
+    parser.add_argument('--use_conll', type=str, required=True)
     args = parser.parse_args()
+    train_and_report(args)
